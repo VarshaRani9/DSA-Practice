e2855a30f912be78b87a8e1fc9dd1b5cccf0b856
VARSHA RANI <127414456+VarshaRani9@users.noreply.github.com>
2023-09-17 09:57:27 +0530

    Create StackUsingQueue.java

---
+++ Stack/StackUsingQueue.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackUsingQueue {
+
+	Queue<Integer> queue1;
+	Queue<Integer> queue2;
+	public StackUsingQueue(){
+		queue1 = new LinkedList<>();
+		queue2 = new LinkedList<>();
+	}
+	public void push(int x) {
+		queue2.add(x);
+		while(queue1.isEmpty()) {
+			queue2.add(queue1.remove());
+		}
+		Queue<Integer> temp = queue1;
+		queue1 = queue2;
+		queue2 = temp;
+	}
+	public int pop() {
+		if(isEmpty()) {
+			throw new RuntimeException ("Empty !");
+		}
+		return queue1.remove();
+	}
+	
+	public boolean isEmpty() {
+		return queue1.isEmpty();
+	}
+	public int size() {
+		return queue1.size();
+	}
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		System.out.println(stack.size());
+		stack.pop();
+		System.out.println(stack.size());
+	}
+
+}
