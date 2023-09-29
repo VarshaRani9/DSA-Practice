9d4cab24742a95239de2522a08ef9e1970b60544
VARSHA RANI <127414456+VarshaRani9@users.noreply.github.com>
2023-09-29 19:50:01 +0530

    Create GFG_DelWithoutHeadPtr.java

---
+++ LinkedList/GFG_DelWithoutHeadPtr.java
@@ -0,0 +1,25 @@
+
+/*
+class Node
+{
+	int data ;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+
+//Function to delete a node without any reference to head pointer.
+class DelWithoutHeadPtr
+{
+    void deleteNode(Node del)
+    {
+         // Your code here
+         del.data = del.next.data;
+         del.next = del.next.next;
+         
+    }
+}
